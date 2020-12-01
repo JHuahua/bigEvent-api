a0d8285d3996139d1d29f8e65dad293fce817b2a
huahua <2023818989@qq.com>
2020-12-01 19:23:19 +0800

    删除文章分类接口完成

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const jwt = require('express-jwt')
 // 导入路由模块
 const loginRouter = require(path.join(__dirname, 'routers/login-router.js'))
 const userRouter = require(path.join(__dirname, 'routers/user-router.js'))
+const articleRouter = require(path.join(__dirname, 'routers/article-router.js'))
 
 const app = express()
 
+// 解析token并验证token的合法性，如果解析失败直接返回错误状态401
+// 以/my开头的都需要验证token，从token中解析出用户id，然后以user属性的方法添加到req对象中
+// req.user = {id: 1}
 app.use(jwt({ secret: 'bigevent' }).unless({ path: /^\/api/ }));
 
 // 处理客户端请求post参数
@@ -29,6 +33,7 @@ app.listen(8888, () => {
 
 app.use('/api', loginRouter)
 app.use('/my', userRouter)
+app.use('/my', articleRouter)
 
 app.use((err, req, res, next) => {
     if (err) {
---
+++ routers/article-router.js
@@ -0,0 +1,76 @@
+/* 
+    文章类别模块
+*/
+
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+const db = require(path.join(__dirname, '../common.js'))
+
+// 获取文章类别列表接口
+router.get('/article/cates', async (req, res) => {
+    // 1.从数据库查询所有文章信息
+    let sql = 'select * from myarticle'
+    let ret = await db.operateDb(sql, null)
+    // 2.根据查询信息返回结果
+    if (ret && ret.length > 0) {
+        res.json({
+            status: 0,
+            message: '获取文章类别列表成功',
+            data: ret
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '获取文章类别列表失败'
+        })
+    }
+})
+
+
+// 新增文章分类接口
+router.post('/article/addcates', async (req, res) => {
+    // 1.获取信息
+    let params = req.body
+    // 2.添加数据到数据库
+    let sql = 'insert into myarticle set ?'
+    let ret = await db.operateDb(sql, params)
+    // 3.根据添加信息返回结果
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '新增文章分类成功'
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '新增文章分类失败'
+        })
+    }
+})
+
+
+// 删除文章分类接口
+router.get('/article/deletecate/:id', async (req, res) => {
+    // 1.获取数据(要删除的文章分类id)
+    let id = req.params.id
+    // 2.根据id删除数据库中对应数据
+    let sql = 'delete from myarticle where id = ?'
+    let ret = await db.operateDb(sql, id)
+    // 3.根据删除信息返回结果
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '删除文章分类成功'
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '删除文章分类失败'
+        })
+    }
+})
+
+
+// 
+module.exports = router
